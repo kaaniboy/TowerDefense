1ed75336b405c5bc07c20d4606876c01bbe33e3d
unknown <kaanaksoyaz@gmail.com>
2016-11-26 15:56:22 -0700

    Added upgrade prices to buttonS

--- Game.java
+++ Game.java
@@ -15,7 +15,6 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
